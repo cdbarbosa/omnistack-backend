d628ef9778acb390eaf62f3aecf039dc0de5cbc6
cdbarbosa <cdbarbosa@atlassian.com>
2020-03-24 19:28:32 -0300

    finalizando backend da aplicaçao

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
+// criando a aplicação
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(cors());
+/**
+ * Metodos HTTP
+ * GET
+ * POST
+ * PUT
+ * DELETE
+ */
+
+/**
+ * Tipos de parâmetros
+ * Query Params - Parâmetros nomeados enviados na rota após  "?" (Filtros, Paginação)
+ * Route Parmas - Parâmetros utilizados para identificar recursos
+ * Request Body - Corpo da requisição, utilizado para criar ou alterar recusos
+ */
+
+app.listen(3333);

c88dbb629a5d5e1101ae8a0c58facb6061ebf135
cdbarbosa <cdbarbosa@atlassian.com>
2020-03-25 22:24:24 -0300

    error por cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 // criando a aplicação
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 /**
  * Metodos HTTP
  * GET
